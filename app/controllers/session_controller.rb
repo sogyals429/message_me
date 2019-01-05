8a17fd3848761df894f380a188b26ed8dcb10a29
sogyal <sogyals429@gmail.com>
2019-01-05 17:47:39 +1100

    Updated sign in form and added session functionality

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,4 +3,21 @@ class SessionController < ApplicationController
 	def new
 		
 	end
+
+	def create
+		user = User.find_by(username:params[:session][:username])
+		if user && user.authenticate(params[:session][:password])
+				session[:user_id] = user.id
+				flash[:success] = "Logged in"
+				redirect_to root_path
+		else
+			flash.now[:error] = "Incorrect details"
+			render 'new'
+		end
+	end
+
+	def destroy
+		session[:user_id] = nil
+		flash[:success] = "You have successfully logged out"
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   root 'chatroom#index'
   get 'login', to:'session#new'
+  post 'login', to:'session#create'
+  delete 'logout', to:'session#destroy'
 end
