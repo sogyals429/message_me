5f6a692e00846ed9455b1357e2595ccc365970a5
sogyal <sogyals429@gmail.com>
2019-01-05 18:07:31 +1100

    Restrict views from controller layer

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,7 +1,9 @@
 class SessionController < ApplicationController
-	
+	before_action :logged_in_redirect,only:[:new,:create]
+
+
+
 	def new
-		
 	end
 
 	def create
@@ -19,5 +21,16 @@ class SessionController < ApplicationController
 	def destroy
 		session[:user_id] = nil
 		flash[:success] = "You have successfully logged out"
+		redirect_to login_path
 	end
+
+	private
+	
+	def logged_in_redirect
+		if logged_in?
+			flash[:error] = "You are already logged in."
+			redirect_to root_path
+		end
+	end
+
 end
