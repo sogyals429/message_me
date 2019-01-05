5485055ab0ec0600884208172d4b9b80a83b428f
sogyal <sogyals429@gmail.com>
2019-01-05 14:49:19 +1100

    Added users and messages resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username:"sam",password:"password")
+User.create(username:"johnsnow",password:"johnsnow123")
+User.create(username:"johndoe",password:"johndoe")
+User.create(username:"steve",password:"steve123")
+User.create(username:"samsmith",password:"samsmith123")
+
+Message.create(body:"Hi John",user_id:1)
+Message.create(body:"Hi Sam",user_id:2)
+Message.create(body:"Hi guys",user_id:3)
+Message.create(body:"Whats up!!",user_id:4)
